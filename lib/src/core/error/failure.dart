cfca2719795136efa4df8a6aaa2ffb07fa8502b3
Kwame Adusei Fosu <44390350+Kriss-Kross33@users.noreply.github.com>
2021-08-21 18:51:55 +0000

    Update: Create subclasses of Exception class.
    Create `ServerException` to be thrown when there is an error when
    fetching data from the remote data source and `CacheException` when
    there is an error fetching from the cache.

--- lib/src/core/error/failure.dart
+++ lib/src/core/error/failure.dart
@@ -1,1 +1,12 @@
-abstract class Failure {}
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class ServerFailure extends Failure {}
+
+/// Failure returned when there is there is a
+/// [CacheException] thrown.
+class CacheFailure extends Failure {}
