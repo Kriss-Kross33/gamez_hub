02d54b758da8c5aa74e5c36b591b24cad9a30e70
Kwame Adusei Fosu <44390350+Kriss-Kross33@users.noreply.github.com>
2021-08-21 18:31:26 +0000

    Implement NetworkInfo to check internet connectivity

--- lib/src/core/network/network_info.dart
+++ lib/src/core/network/network_info.dart
@@ -1,3 +1,14 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
 abstract class NetworkInfo {
   Future<bool> get hasConnection;
+}
+
+class NetworkInfoImpl extends NetworkInfo {
+  final InternetConnectionChecker internetConnectionChecker;
+
+  NetworkInfoImpl(this.internetConnectionChecker);
+
+  @override
+  Future<bool> get hasConnection => internetConnectionChecker.hasConnection;
 }
