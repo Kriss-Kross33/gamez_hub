7f51545d5736654dc47f0d852d818a951178e401
Kwame Adusei Fosu <44390350+Kriss-Kross33@users.noreply.github.com>
2021-08-21 18:25:19 +0000

    Implement local data source

--- lib/src/core/games/data/datasources/game_local_data_source.dart
+++ lib/src/core/games/data/datasources/game_local_data_source.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:gamez_hub/src/core/constants/secure_storage_consts.dart';
+import 'package:gamez_hub/src/core/error/exception.dart';
 import 'package:gamez_hub/src/core/games/data/models/game_model.dart';
 
 abstract class GameLocalDataSource {
@@ -9,4 +14,30 @@ abstract class GameLocalDataSource {
   /// Cache the list of [GameModel] when data is gotten from
   /// remote data source.
   Future<void> addGameListToCache(List<GameModel> games);
+}
+
+class GameLocalDataSourceImpl extends GameLocalDataSource {
+  final FlutterSecureStorage secureStorage;
+
+  GameLocalDataSourceImpl({required this.secureStorage});
+  @override
+  Future<void> addGameListToCache(List<GameModel> games) async {
+    String gamesListString = json.encode(games.map((e) => e.toJson()).toList());
+    await secureStorage.write(
+        key: SecureStorage.FETCH_GAMES_LIST, value: gamesListString);
+  }
+
+  @override
+  Future<List<GameModel>> fetchCahedGames() async {
+    final gamesListString =
+        await secureStorage.read(key: SecureStorage.FETCH_GAMES_LIST);
+    if (gamesListString != null) {
+      List<GameModel> games = (json.decode(gamesListString) as List)
+          .map((e) => GameModel.fromJson(e))
+          .toList();
+      return games;
+    } else {
+      throw CacheException();
+    }
+  }
 }
