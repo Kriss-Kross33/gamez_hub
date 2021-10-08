d6525af414c2378844a0fac6d60ca3ad8e7d6f2f
Kwame Adusei Fosu <44390350+Kriss-Kross33@users.noreply.github.com>
2021-10-08 15:10:07 +0000

    Create `SimpleBlocObserver` class to monitor events and app states.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gamez_hub/src/app.dart';
+import 'package:gamez_hub/src/core/service_locator/service_locator.dart';
 
-void main() {
+Future<void> main() async {
+  SimpleBlocObserver observer = SimpleBlocObserver();
+  await setupLocator();
   runApp(App());
+}
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+  }
 }
--- lib/src/core/themes/theme_text.dart
+++
@@ -1,7 +0,0 @@
-// import 'package:flutter/material.dart';
-
-// class ThemeText {
-//   const ThemeText._();
-
-//   static TextTheme get _boltTextTheme => 
-// }
