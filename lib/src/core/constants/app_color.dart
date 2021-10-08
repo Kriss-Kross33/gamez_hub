63b887bc57def08711c0d28e65c073bc5f4d10db
Kwame Adusei Fosu <44390350+Kriss-Kross33@users.noreply.github.com>
2021-10-08 14:57:32 +0000

    Class that will define app color codes

--- lib/src/core/constants/app_color.dart
+++ lib/src/core/constants/app_color.dart
@@ -3,21 +3,22 @@ import 'package:flutter/material.dart';
 class AppColor {
   const AppColor._();
 
-  static const Color acadia = Color(0xFF1B1404);
-  static const Color black = Color(0xFF333333);
-  static const Color lemonGreen = Color(0xFF57d772);
-  static const Color limeGreen = Color(0xFF32CD32);
-  static const Color darkCharcoal = Color(0xFF333333);
-  static const Color chineseBlack = Color(0xFF101010);
-  static const Color athensGrey = Color(0xFFE1E4E8);
-  static const Color goldDeep = Color(0xFFFDBD39);
-  static const Color lightGrey = Color(0xFFC9CEd6);
-  static const Color violetBG = Color(0xff000028);
-  static const Color whiteSmoke = Color(0xFFF0F0F0);
-  static const Color whiteBG = Color(0xfff7f7f7);
-  static const Color white = Color(0xffffffff);
-  static const Color violet = Color(0xff0002b6);
-  static const Color orange = Color(0xffea4c29);
-  static const Color darkGrey = Color(0xFF5c5c5c);
-  static const Color lightDarkGrey = Color(0xFF858585);
+  static const Color buttonColor = Color(0xFF8859C8);
+  static const Color buttonDisabledColor = Colors.grey;
+  static const Color iconColor = Color(0xFFB46FFE);
+
+  //* Dark Mode
+  static const Color darkPrimaryColor = Color(0xFF24242E);
+  static const Color darkPrimaryVariantConr = Color(0xFF1A1A22);
+  static const Color darkSecondaryColor = Color(0xFF9F5CE5);
+  static const Color darkSecondaryVariantColor = Color(0xFF4A4D6B);
+
+  /// Light color when in dark mode
+  static const Color darkOnPrimaryColor = lightPrimaryColor;
+
+  //* Light Mode
+  static const Color lightPrimaryColor = Color(0xFFF7F7F7);
+  static const Color lightPrimaryVariantColor =
+      Colors.white; //Color(0xFFAEB0B1);
+  static const Color lightSecondaryColor = Color(0xFF9F5CE5);
 }
