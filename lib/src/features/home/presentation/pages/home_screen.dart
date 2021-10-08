94246efed17932733dcdefd75817d017b8cb104f
Kwame Adusei Fosu <44390350+Kriss-Kross33@users.noreply.github.com>
2021-10-08 15:09:04 +0000

    Add  widget to .

--- lib/src/features/home/presentation/pages/home_screen.dart
+++ lib/src/features/home/presentation/pages/home_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:gamez_hub/src/core/gamez_hub_app/widgets/widget.dart';
+import 'package:gamez_hub/src/features/home/presentation/widgets/widgets.dart';
+import 'package:gamez_hub/src/responsive_layout.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      drawer: NavigationDrawer(),
+      body: ListView(
+        children: [
+          ResponsiveLayout(
+            desktop: Container(),
+            tablet: Container(),
+            phone: PhoneBody(),
+          ),
+        ],
+      ),
+    );
   }
 }
