d74c3305c4d5b05e889f52437e1cd17967266e2e
Kwame Adusei Fosu <44390350+Kriss-Kross33@users.noreply.github.com>
2021-10-08 15:09:52 +0000

    Apply custom theme to MaterialApp.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:gamez_hub/src/core/constants/app_color.dart';
-import 'package:gamez_hub/src/features/home/presentation/pages/home_screen.dart';
+import 'package:gamez_hub/src/core/themes/app_theme.dart';
+
+import 'core/gamez_hub_app/gamez_hub_app.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -8,10 +9,10 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: AppColor.lightGrey,
-      ),
-      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      darkTheme: AppTheme.darkTheme,
+      theme: AppTheme.defaultTheme,
+      home: GamezHubApp(),
     );
   }
 }
