6df71e5da1963185c36d756d74edbf5ac7c986a0
Kwame Adusei Fosu <44390350+Kriss-Kross33@users.noreply.github.com>
2021-10-08 14:59:11 +0000

    Class that defines text used in the UI

---
+++ lib/src/core/constants/text_consts.dart
@@ -0,0 +1,7 @@
+class TextConst {
+  const TextConst._();
+
+  static const String popularGamesText = 'Popular';
+  static const String comingSoonGamesText = 'Coming Soon';
+  static const String newGamesText = 'New Games';
+}
