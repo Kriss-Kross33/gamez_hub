e2d9125f62c31b2de64d7d6110280ff30f6cfa31
Kwame Adusei Fosu <44390350+Kriss-Kross33@users.noreply.github.com>
2021-10-08 14:57:03 +0000

    Class that will define error message strings

---
+++ lib/src/core/constants/error_consts.dart
@@ -0,0 +1,6 @@
+class ErrorConsts {
+  const ErrorConsts._();
+
+  static const String SERVER_ERROR_MESSAGE = 'Server Error, try again later';
+  static const String CACHE_ERROR_MESSAGE = 'Could not fing cache';
+}
