c438d5fc7826937d3be00f5f321499586ee49c6e
Kwame Adusei Fosu <44390350+Kriss-Kross33@users.noreply.github.com>
2021-10-11 03:27:00 +0000

    Clear cached data on app launch

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:gamez_hub/src/core/themes/app_theme.dart';
 
+import 'core/constants/secure_storage_consts.dart';
 import 'core/gamez_hub_app/gamez_hub_app.dart';
+import 'core/service_locator/service_locator.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
+
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    clearGameCache();
+  }
+
+  Future<void> clearGameCache() async {
+    FlutterSecureStorage secureStorage = locator<FlutterSecureStorage>();
+    await secureStorage.delete(key: SecureStorage.FETCH_GAMES_LIST);
+  }
 
   @override
   Widget build(BuildContext context) {
