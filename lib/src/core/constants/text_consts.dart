6da08458cd290eb7c33aad8c3e36986191e8f112
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-02-20 13:37:44 +0000

    add game genre String to

--- lib/src/core/constants/text_consts.dart
+++ lib/src/core/constants/text_consts.dart
@@ -4,5 +4,7 @@ class TextConst {
   static const String popularGamesText = 'Popular';
   static const String comingSoonGamesText = 'Coming Soon';
   static const String newGamesText = 'New Games';
+  static const String actionGames = 'Live Stream';
+  static const String indieGames = 'Indie Games';
   static const String liveStreamText = 'Live Stream';
 }
