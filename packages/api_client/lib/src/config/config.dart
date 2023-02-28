0260f2dba451eba47643f07a64d82b4da72c07fa
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-02-28 11:06:57 +0000

    remove redundant files

--- packages/api_client/lib/src/config/config.dart
+++
@@ -1,2 +0,0 @@
-export 'api_config.dart';
-export 'key.dart';
--- packages/api_client/lib/src/config/key.dart
+++
@@ -1,3 +0,0 @@
-const Map<String, String> apiKey = {'KEY': '91e1f9acec4141d18806c124f76724c3'};
-
-const Map<String, String> twitchKey = {};
