d55f9713947c7802012acc48a41eafa02583a87c
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-03-02 15:44:38 +0000

    create  class to represent success response

---
+++ packages/authentication_repository/lib/src/domain/entities/entities.dart
@@ -0,0 +1,2 @@
+export 'success.dart';
+export 'user_entity.dart';
---
+++ packages/authentication_repository/lib/src/domain/entities/success.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+/// Represents a successful response from the server.
+class Success extends Equatable {
+  ///
+  static Success instance = Success();
+
+  @override
+  List<Object> get props => [];
+}
